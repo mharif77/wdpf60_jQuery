80d672f086049c64560cdb52020d5ea100cd5ee0
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-09-09 13:44:28 +0600

    bootstrap customization

---
+++ 09 september 2024/admin/partial/left_panel.php
@@ -0,0 +1,31 @@
+<div class="br-logo"><a href=""><span>[</span>bracket <i>plus</i><span>]</span></a></div>
+  <div class="br-sideleft sideleft-scrollbar">
+    <label class="sidebar-label">Navigation</label>
+    <ul class="br-sideleft-menu">
+      <li class="br-menu-item">
+        <a href="dashboard.php" class="br-menu-link active">
+          <i class="menu-item-icon icon ion-ios-home-outline tx-24"></i>
+          <span class="menu-item-label">Dashboard</span>
+        </a><!-- br-menu-link -->
+      </li><!-- br-menu-item -->
+     
+      <li class="br-menu-item">
+        <a href="#" class="br-menu-link with-sub">
+          <i class="menu-item-icon icon ion-ios-photos-outline tx-20"></i>
+          <span class="menu-item-label">students</span>
+        </a><!-- br-menu-link -->
+        <ul class="br-menu-sub">
+          <li class="sub-item"><a href="students.php" class="sub-link">students_list</a></li>
+          <li class="sub-item"><a href="student_entry.php" class="sub-link">new_student</a></li>
+          
+        </ul>
+      </li>
+ 
+    </ul><!-- br-sideleft-menu -->
+
+    
+
+   
+
+    <br>
+  </div><!-- br-sideleft -->
