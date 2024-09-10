49edc92c86ad7bcb34a4da521996f992c4792acc
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-09-10 14:36:00 +0600

    Zegva responsive admin project customization

--- 09 september 2024/admin/student_entry.php
+++ 09 september 2024/admin/student_entry.php
@@ -61,111 +61,122 @@ if (!isset($_SESSION['email'])) {
 
     <!-- ########## START: MAIN PANEL ########## -->
     <div class="br-mainpanel">
-      <div class="br-pageheader">
-        <nav class="breadcrumb pd-0 mg-0 tx-12">
-          <a class="breadcrumb-item" href="index.html">Bracket</a>
-          <a class="breadcrumb-item" href="#">Forms</a>
-          <span class="breadcrumb-item active">Form Layouts</span>
-        </nav>
-      </div><!-- br-pageheader -->
-      <div class="br-pagetitle">
-        <i class="icon ion-ios-gear-outline"></i>
-        <div>
-          <h4>Form Layouts</h4>
-          <p class="mg-b-0">Forms are used to collect user information with different element types of input, select, checkboxes, radios and more.</p>
-        </div>
-      </div><!-- d-flex -->
+        <div class="br-pageheader">
+            <nav class="breadcrumb pd-0 mg-0 tx-12">
+                <a class="breadcrumb-item" href="index.html">Bracket</a>
+                <a class="breadcrumb-item" href="#">Forms</a>
+                <span class="breadcrumb-item active">Form Layouts</span>
+            </nav>
+        </div><!-- br-pageheader -->
+        <div class="br-pagetitle">
+            <i class="icon ion-ios-gear-outline"></i>
+            <div>
+                <h4>Form Layouts</h4>
+                <p class="mg-b-0">Forms are used to collect user information with different element types of input,
+                    select, checkboxes, radios and more.</p>
+            </div>
+        </div><!-- d-flex -->
 
-      <div class="br-pagebody">
-        <div class="br-section-wrapper">
-         
+        <div class="br-pagebody">
+            <div class="br-section-wrapper">
 
-          <!-- form-layout -->
 
-        
+                <!-- form-layout -->
 
-        
-          <div class="row mg-t-80">
-            <div class="col-xl-12">
-              <div class="form-layout form-layout-4">
-                <h6 class="br-section-label">Left Label Alignment</h6>
-                <p class="br-section-text">A basic form where labels are aligned in left.</p>
-                <div class="row">
-                  <label class="col-sm-4 form-control-label">ID : <span class="tx-danger">*</span></label>
-                  <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                    <input type="text" class="form-control" placeholder="Enter your id">
-                  </div>
+
+
+
+                <div class="row mg-t-80">
+                    <div class="col-xl-12">
+                        <div class="form-layout form-layout-4">
+                            <h6 class="br-section-label">Left Label Alignment</h6>
+                            <p class="br-section-text">A basic form where labels are aligned in left.</p>
+                            <div class="row">
+                                <label class="col-sm-4 form-control-label">ID : <span class="tx-danger">*</span></label>
+                                <div class="col-sm-8 mg-t-10 mg-sm-t-0">
+                                    <input type="text" class="form-control" placeholder="Enter your id">
+                                </div>
+                            </div><!-- row -->
+
+                            <div class="row mg-t-20">
+                                <label class="col-sm-4 form-control-label">NAME: <span
+                                        class="tx-danger">*</span></label>
+                                <div class="col-sm-8 mg-t-10 mg-sm-t-0">
+                                    <input type="text" class="form-control" placeholder="Enter your name">
+                                </div>
+                            </div>
+                            <div class="row mg-t-20">
+                                <label class="col-sm-4 form-control-label">GENDER: <span
+                                        class="tx-danger">*</span></label>
+                                <div class="col-sm-8 mg-t-10 mg-sm-t-0">
+                                    Male <input type="radio" class="form-control-m" name="gender" value="Male">
+                                    Female <input type="radio" class="form-control-m" name="gender" value="Female">
+                                </div>
+                            </div>
+                            <div class="row mg-t-20">
+                                <label class="col-sm-4 form-control-label">DOB: <span class="tx-danger">*</span></label>
+                                <div class="col-sm-8 mg-t-10 mg-sm-t-0">
+                                    <input type="text" class="form-control" placeholder="Enter your date of birth">
+                                </div>
+                            </div>
+                            <div class="row mg-t-20">
+                                <label class="col-sm-4 form-control-label">EMAIL: <span
+                                        class="tx-danger">*</span></label>
+                                <div class="col-sm-8 mg-t-10 mg-sm-t-0">
+                                    <input type="text" class="form-control" placeholder="Enter your email">
+                                </div>
+                            </div>
+                            <div class="row mg-t-20">
+                                <label class="col-sm-4 form-control-label">PHONE: <span
+                                        class="tx-danger">*</span></label>
+                                <div class="col-sm-8 mg-t-10 mg-sm-t-0">
+                                    <input type="text" class="form-control" placeholder="Enter your phone">
+                                </div>
+                            </div>
+                            <div class="row mg-t-20">
+                                <label class="col-sm-4 form-control-label">ADDRESS: <span
+                                        class="tx-danger">*</span></label>
+                                <div class="col-sm-8 mg-t-10 mg-sm-t-0">
+                                    <textarea rows="2" class="form-control" placeholder="Enter your address"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-layout-footer mg-t-30">
+                                <button class="btn btn-info">Submit Form</button>
+                                <button class="btn btn-secondary">Cancel</button>
+                            </div><!-- form-layout-footer -->
+                        </div><!-- form-layout -->
+                    </div><!-- col-6 -->
+
                 </div><!-- row -->
-                
-                <div class="row mg-t-20">
-                  <label class="col-sm-4 form-control-label">NAME: <span class="tx-danger">*</span></label>
-                  <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                    <input type="text" class="form-control" placeholder="Enter your name">
-                  </div>
-                </div>
-                <div class="row mg-t-20">
-                  <label class="col-sm-4 form-control-label">GENDER: <span class="tx-danger">*</span></label>
-                  <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                    <input type="text" class="form-control" placeholder="Enter your gender">
-                  </div>
-                </div>
-                <div class="row mg-t-20">
-                  <label class="col-sm-4 form-control-label">DOB: <span class="tx-danger">*</span></label>
-                  <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                    <input type="text" class="form-control" placeholder="Enter your date of birth">
-                  </div>
-                </div>
-                <div class="row mg-t-20">
-                  <label class="col-sm-4 form-control-label">EMAIL: <span class="tx-danger">*</span></label>
-                  <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                    <input type="text" class="form-control" placeholder="Enter your email">
-                  </div>
-                </div>
-                <div class="row mg-t-20">
-                  <label class="col-sm-4 form-control-label">PHONE: <span class="tx-danger">*</span></label>
-                  <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                    <input type="text" class="form-control" placeholder="Enter your phone">
-                  </div>
-                </div>
-                <div class="row mg-t-20">
-                  <label class="col-sm-4 form-control-label">ADDRESS: <span class="tx-danger">*</span></label>
-                  <div class="col-sm-8 mg-t-10 mg-sm-t-0">
-                    <textarea rows="2" class="form-control" placeholder="Enter your address"></textarea>
-                  </div>
-                </div>
-                <div class="form-layout-footer mg-t-30">
-                  <button class="btn btn-info">Submit Form</button>
-                  <button class="btn btn-secondary">Cancel</button>
-                </div><!-- form-layout-footer -->
-              </div><!-- form-layout -->
-            </div><!-- col-6 -->
-            
-          </div><!-- row -->
 
 
-          
-
-        
 
 
-         
-          
 
-          
 
-        </div><!-- br-section-wrapper -->
-      </div><!-- br-pagebody -->
-      <footer class="br-footer">
-        <div class="footer-left">
-          <div class="mg-b-2">Copyright &copy; 2017. Bracket Plus. All Rights Reserved.</div>
-          <div>Attentively and carefully made by ThemePixels.</div>
-        </div>
-        <div class="footer-right d-flex align-items-center">
-          <span class="tx-uppercase mg-r-10">Share:</span>
-          <a target="_blank" class="pd-x-5" href="https://www.facebook.com/sharer/sharer.php?u=http%3A//themepixels.me/bracketplus/intro"><i class="fab fa-facebook tx-20"></i></a>
-          <a target="_blank" class="pd-x-5" href="https://twitter.com/home?status=Bracket%20Plus,%20your%20best%20choice%20for%20premium%20quality%20admin%20template%20from%20Bootstrap.%20Get%20it%20now%20at%20http%3A//themepixels.me/bracketplus/intro"><i class="fab fa-twitter tx-20"></i></a>
-        </div>
-      </footer>
+
+
+
+
+
+
+            </div><!-- br-section-wrapper -->
+        </div><!-- br-pagebody -->
+        <footer class="br-footer">
+            <div class="footer-left">
+                <div class="mg-b-2">Copyright &copy; 2017. Bracket Plus. All Rights Reserved.</div>
+                <div>Attentively and carefully made by ThemePixels.</div>
+            </div>
+            <div class="footer-right d-flex align-items-center">
+                <span class="tx-uppercase mg-r-10">Share:</span>
+                <a target="_blank" class="pd-x-5"
+                    href="https://www.facebook.com/sharer/sharer.php?u=http%3A//themepixels.me/bracketplus/intro"><i
+                        class="fab fa-facebook tx-20"></i></a>
+                <a target="_blank" class="pd-x-5"
+                    href="https://twitter.com/home?status=Bracket%20Plus,%20your%20best%20choice%20for%20premium%20quality%20admin%20template%20from%20Bootstrap.%20Get%20it%20now%20at%20http%3A//themepixels.me/bracketplus/intro"><i
+                        class="fab fa-twitter tx-20"></i></a>
+            </div>
+        </footer>
     </div><!-- br-mainpanel -->
     <!-- ########## END: MAIN PANEL ########## -->
 
