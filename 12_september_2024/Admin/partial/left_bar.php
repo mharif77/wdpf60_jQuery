5d186363bcafcbdaf45ae5e86766ca7adb018a34
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-09-12 13:42:57 +0600

    fronted er kaj

---
+++ 12_september_2024/Admin/partial/left_bar.php
@@ -0,0 +1,38 @@
+<div class="left side-menu">
+            <div class="slimscroll-menu" id="remove-scroll">
+
+                <!--- Sidemenu -->
+                <div id="sidebar-menu">
+                    <!-- Left Menu Start -->
+                    <ul class="metismenu" id="side-menu">
+                        <li class="menu-title">Menu</li>
+                        <li>
+                            <a href="dashboard.php" class="waves-effect">
+                                <i class="dripicons-meter"></i><span
+                                    class="badge badge-info badge-pill float-right">9+</span> <span> Dashboard </span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="javascript:void(0);" class="waves-effect"><i class="person"></i><span>
+                            doctor <span class="float-right menu-arrow"><i
+                                            class="mdi mdi-chevron-right"></i></span> </span></a>
+                            <ul class="submenu">
+                                <li><a href="Specialization.php">Specialization</a></li>
+                                <li><a href="add_doctor.php">add_doctor</a></li>
+                                <li><a href="manage_doctor.php">manage_doctor</a></li>
+                            </ul>
+                        </li>
+
+                        
+
+                    </ul>
+
+                </div>
+                <!-- Sidebar -->
+                <div class="clearfix"></div>
+
+            </div>
+            <!-- Sidebar -left -->
+
+        </div>
